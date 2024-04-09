de2823ddd979826874deca744f7136c90b0d252d
Shubhamsss123 <108915629+Shubhamsss123@users.noreply.github.com>
2024-04-09 20:51:23 +0530

    added three transformation in guassian blur

--- add_gaussian_blur.py
+++ add_gaussian_blur.py
@@ -3,21 +3,26 @@ import os
 import numpy as np 
 from tqdm import tqdm
 
-src_dir = '/content/data/clear_images'
+root=os.path.dirname(os.getcwd())
+# src_dir = '/content/data/clear_images'
+src_dir=os.path.join(root,'clear_images')
+
 images = os.listdir(src_dir)
-dst_dir = '/content/blur_images'
+# dst_dir = '/content/blur_images'
+dst_dir=os.path.join(root,'blur_data')
 
 kernels=[((3,3),0.3),((7,7),1),((11,11),1.6)]
 for i, img in tqdm(enumerate(images), total=len(images)):
     for el in os.listdir( '/content/data/clear_images/'+images[i]):
       img = cv2.imread(f"{src_dir}/{images[i]}/{el}")
 
-      temp=np.random.randint(0,3)
+      # temp=np.random.randint(0,3)
       # print(img.shape, kernels[temp][0], kernels[temp][1],sep='==>')
       # add gaussian blurring
       # print(img.shape)
       # print(temp,kernels[temp][0])
-      blur = cv2.GaussianBlur(img, kernels[temp][0], kernels[temp][1])
-      cv2.imwrite(f"{dst_dir}/{images[i]}.png", blur)
+      for temp in range(3):
+          blur = cv2.GaussianBlur(img, kernels[temp][0], kernels[temp][1])
+          cv2.imwrite(f"{dst_dir}/{images[i]}_{temp}.png", blur)
 
 print('DONE')
