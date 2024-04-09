c4565f941df271e567a1a9085dc1579df717d8bb
Shubhamsss123 <108915629+Shubhamsss123@users.noreply.github.com>
2024-04-09 20:54:11 +0530

    added for loop in clear.py

--- clear_folder.py
+++ clear_folder.py
@@ -3,17 +3,19 @@ import os
 import numpy as np 
 from tqdm import tqdm
 
-src_dir = '/content/data/clear_images'
+root=os.path.dirname(os.getcwd())
+
+src_dir = os.path.join(root,'clear_images')
 images = os.listdir(src_dir)
-dst_dir = '/content/clear_data'
+dst_dir = os.path.join(root,'clear_data')
 
 kernels=[((3,3),0.3),((7,7),1),((11,11),1.6)]
 for i, img in tqdm(enumerate(images), total=len(images)):
     for el in os.listdir( '/content/data/clear_images/'+images[i]):
-      img = cv2.imread(f"{src_dir}/{images[i]}/{el}")
 
+      for temp in range(3):
+          img = cv2.imread(f"{src_dir}/{images[i]}/{el}")
     
- 
-      cv2.imwrite(f"{dst_dir}/{images[i]}.png", img)
+          cv2.imwrite(f"{dst_dir}/{images[i]}_{temp}.png", img)
 
 print('DONE')
